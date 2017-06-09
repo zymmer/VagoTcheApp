d5440dfdb9c68153ba7bb909eb5f8dd9c2fb9a83
guilherme <guizymmer@gmail.com>
2017-06-09 00:12:04 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/MapsActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/MapsActivity.java
@@ -61,7 +61,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Testes Parquimetros
         addParquimetroAugustoSevero();
