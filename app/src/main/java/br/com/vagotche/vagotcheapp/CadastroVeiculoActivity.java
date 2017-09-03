0914d74a388dcd0dcd1d24b4800d9ddb7288e8d2
guilherme <guizymmer@gmail.com>
2017-09-03 12:18:34 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/CadastroVeiculoActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/CadastroVeiculoActivity.java
@@ -11,7 +11,6 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
