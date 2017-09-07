dda5ba2bead2b48fac150c9154181e5b7ee1cceb
guilherme <guizymmer@gmail.com>
2017-09-07 10:35:38 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
@@ -1,17 +1,58 @@
 package br.com.vagotche.vagotcheapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
-public class ConfiguracaoAlertasActivity extends AppCompatActivity {
+public class ConfiguracaoAlertasActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnCancelar, btnSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao_alertas);
+
+        btnCancelar = (Button) findViewById(R.id.btnCancelarConf);
+        btnSalvar = (Button) findViewById(R.id.btnSalvarConf);
+
+        // Button listeners
+        btnCancelar.setOnClickListener(this);
+        btnSalvar.setOnClickListener(this);
+    }
+
+    private void salvar() {
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()){
+
+            String nome = editNome.getText().toString();
+            String cpf = editCPF2.getText().toString();
+            String email = editEmail1.getText().toString();
+            String senha = editPassword2.getText().toString();
+
+
+            if(nome.isEmpty() || cpf.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                alert("Nenhum campo pode estar vazio");
+            }else {
+                url = "http://fabrica.govbrsul.com.br/vagotche/index.php/Cadastro/CadastrarLogin";
+
+                parametros = "nome=" + nome + "&cpf=" + cpf + "&email=" + email + "&senha=" + senha;
+
+                new CadastroActivity.SolicitaDados().execute(url);
+            }
+
+        } else {
+            alert("Nenhuma conexão foi detectada");
+        }
     }
 
 
@@ -34,6 +75,18 @@ public class ConfiguracaoAlertasActivity extends AppCompatActivity {
                 // I'm lactose intolerant
                 break;
             // TODO: Veggie sandwich
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCancelarConf:
+                finish();
+                break;
+            case R.id.btnSalvarConf:
+                salvar();
+                break;
         }
     }
 
