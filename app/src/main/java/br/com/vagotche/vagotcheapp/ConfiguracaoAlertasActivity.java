3411f9e88ef34948e6e5e6198c3f16de6c7527ca
guilherme <guizymmer@gmail.com>
2017-09-07 10:14:58 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
@@ -2,12 +2,39 @@ package br.com.vagotche.vagotcheapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class ConfiguracaoAlertasActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao_alertas);
     }
+
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.alerta1:
+                if (checked)
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.checkbox_cheese:
+                if (checked)
+                // Cheese me
+            else
+                // I'm lactose intolerant
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
+
 }
