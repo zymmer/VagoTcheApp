e8510affdc306b2ea5c318e1b8d0630ac4dc0a34
guilherme <guizymmer@gmail.com>
2017-09-07 11:44:12 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/ConfiguracaoAlertasActivity.java
@@ -1,22 +1,36 @@
 package br.com.vagotche.vagotcheapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class ConfiguracaoAlertasActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Variaveis
+    int cdUsuario;
     Button btnCancelar, btnSalvar;
+    String url = "";
+    String parametros = "";
+    Boolean alerta1, alerta2, alerta3, alerta4;
+
+    private void alert(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracao_alertas);
+
+        cdUsuario = getIntent().getIntExtra("id_usuario", 0);
 
         btnCancelar = (Button) findViewById(R.id.btnCancelarConf);
         btnSalvar = (Button) findViewById(R.id.btnSalvarConf);
@@ -34,27 +48,40 @@ public class ConfiguracaoAlertasActivity extends AppCompatActivity implements Vi
 
         if (networkInfo != null && networkInfo.isConnected()){
 
-            String nome = editNome.getText().toString();
-            String cpf = editCPF2.getText().toString();
-            String email = editEmail1.getText().toString();
-            String senha = editPassword2.getText().toString();
+                url = "http://fabrica.govbrsul.com.br/vagotche/index.php/ConfAlertas/ConfigurarAlertas";
 
+                parametros =    "cdUsuario=" + cdUsuario + "&alerta1=" + alerta1 + "&alerta2=" + alerta2 +
+                                "&alerta3=" + alerta3 + "&alerta4=" + alerta4;
 
-            if(nome.isEmpty() || cpf.isEmpty() || email.isEmpty() || senha.isEmpty()){
-                alert("Nenhum campo pode estar vazio");
-            }else {
-                url = "http://fabrica.govbrsul.com.br/vagotche/index.php/Cadastro/CadastrarLogin";
-
-                parametros = "nome=" + nome + "&cpf=" + cpf + "&email=" + email + "&senha=" + senha;
-
-                new CadastroActivity.SolicitaDados().execute(url);
+                new SolicitaDados().execute(url);
             }
 
-        } else {
-            alert("Nenhuma conexão foi detectada");
-        }
     }
 
+    private class SolicitaDados extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return ConexaoApp.postDados(urls[0], parametros);
+
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+
+            //teste
+            alert(resultado);
+
+            if (resultado.contains("conf_atualizada")) {
+                alert("Configurações Atualizadas");
+
+                Intent it = new Intent(ConfiguracaoAlertasActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+
+        }
+    }
 
     public void onCheckboxClicked(View view) {
         // Is the view now checked?
@@ -64,17 +91,28 @@ public class ConfiguracaoAlertasActivity extends AppCompatActivity implements Vi
         switch(view.getId()) {
             case R.id.alerta1:
                 if (checked)
-                // Put some meat on the sandwich
+                    alerta1 = true;
             else
-                // Remove the meat
+                    alerta1 = false;
                 break;
             case R.id.alerta2:
                 if (checked)
-                // Cheese me
+                    alerta2 = true;
             else
-                // I'm lactose intolerant
+                    alerta2 = false;
                 break;
-            // TODO: Veggie sandwich
+            case R.id.alerta3:
+                if (checked)
+                    alerta3 = true;
+                else
+                    alerta3 = false;
+                break;
+            case R.id.alerta4:
+                if (checked)
+                    alerta4 = true;
+                else
+                    alerta4 = false;
+                break;
         }
     }
 
