1d5b03c3cf79634ca0ce7da2ffa58eac2015a032
guilherme <guizymmer@gmail.com>
2017-09-10 16:36:53 -0300

    Atualizações

--- app/src/main/java/br/com/vagotche/vagotcheapp/Veiculos.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/Veiculos.java
@@ -356,6 +356,11 @@ public class Veiculos {
                 "Volkswagen Passar Variant");
 
     List<String> ano = Arrays.asList (
+            "2007-2008",
+            "2008-2008",
+            "2008-2009",
+            "2009-2009",
+            "2009-2010",
             "2010-2010",
             "2010-2011",
             "2011-2011",
