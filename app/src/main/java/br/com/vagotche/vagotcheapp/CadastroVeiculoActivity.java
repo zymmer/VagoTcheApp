f12e9392fcc23b8c6f6d202cdc6653767bfe5dc7
guilherme <guizymmer@gmail.com>
2017-10-23 23:26:21 -0200

    New

--- app/src/main/java/br/com/vagotche/vagotcheapp/CadastroVeiculoActivity.java
+++ app/src/main/java/br/com/vagotche/vagotcheapp/CadastroVeiculoActivity.java
@@ -16,10 +16,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.kosalgeek.android.json.JsonConverter;
-import com.kosalgeek.asynctask.PostResponseAsyncTask;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
